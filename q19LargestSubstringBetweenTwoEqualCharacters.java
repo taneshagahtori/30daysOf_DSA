7de79844f2ff97bb396f67b1a2cd30a49ffb247a
taneshagahtori <iamtanesha13@gmail.com>
2023-12-31 22:59:07 +0530

    day19

---
+++ q19LargestSubstringBetweenTwoEqualCharacters.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxLengthBetweenEqualCharacters(String s) {
+        Map <Character, Integer> map= new HashMap<>();
+        int max= -1;
+        for(int i=0; i<s.length(); i++){
+            char ch=s.charAt(i);
+            if(map.containsKey(ch)){
+                max= Math.max(max, i-map.get(ch)-1);
+            }
+            else{
+                map.put(ch,i);
+            }
+        }
+        return max;
+    }
+}
